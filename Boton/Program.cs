fe4470c349a9c6c8455d7b28547e28b5d8857c7d
JonathanMov <jafetmv21@gmail.com>
2021-03-10 08:38:47 -0800

    Creacion de Boton sin uso del disenador

---
+++ Boton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            Form bt = new Form() {
+                Size = new Size(500, 500),
+                Text = "Solo soy una form con un boton :)"
+            };
+            bt.Controls.Add(new Button() {
+                Text = "Soy un Boton",
+                AutoSize = true,
+                Location = new Point(200, 200)
+            }
+            );
+            Application.Run(bt);
+        }
+    }
+}
