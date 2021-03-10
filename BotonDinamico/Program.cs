153eabda5bb93402f25ad68c6045e31b17904203
JonathanMov <jafetmv21@gmail.com>
2021-03-10 10:07:11 -0800

    modificacion de animacion de movimiento

--- BotonDinamico/Program.cs
+++ BotonDinamico/Program.cs
@@ -5,36 +5,47 @@ using System.Drawing;
 
 namespace BotonDinamico {
     class Program {
-        static void Main(string[] args) {
-            Application.Run(new Lienzo());
+        static void Main() {
+            Application.Run(new Lienzo() {
+                Size = new Size(500,500),
+                BackColor = Color.Coral,
+                StartPosition = FormStartPosition.CenterScreen,
+                Text = "Mojica Vidal Jonathan Jafet - 19211688"
+            });
         }
     }
     class Lienzo : Form {
-        Button btn;
+        readonly Button btn;
+        int velx = 4, vely = 4;
         public Lienzo() {
-            this.Size = new Size(500, 500);
-            this.BackColor = Color.Aquamarine;
-
             btn = new Button() {
                 BackColor = Color.Brown,
                 ForeColor = Color.White,
-                Text = "Me muevo",
+                Text = "Me\nmuevo",
+                Font = new Font(familyName:"Comic Sans MS", 12),
                 AutoSize = true,
-                Location = new Point(250, 250)
+                Location = new Point(0, 250)
             };
+            this.btn.Height = this.btn.Width;
 
             this.Controls.Add(this.btn);
-
             moverBoton();
         }
         private async void moverBoton() {
-            while (5 > 1) {
+            while (true) {
+                if (this.btn.Right > this.Width - 15)
+                    this.velx = -4;
+                if (this.btn.Left < 0)
+                    this.velx = 4;
 
-                int a = new Random().Next(this.btn.Width / 2, maxValue: this.Width - 50 - ( this.btn.Width / 2 ));
-                int b = new Random().Next(this.btn.Height / 2, maxValue: this.Height - 40 - ( this.btn.Height / 2 ));
+                if (this.btn.Top < 0)
+                    this.vely = 4;
+                if (this.btn.Bottom > this.Height - 35)
+                    this.vely = -4;
 
-                await Task.Delay(1000);
-                this.btn.Location = new Point(a, b);
+                this.btn.Left += this.velx;
+                this.btn.Top += this.vely;
+                await Task.Delay(20);
             }
         }
     }
