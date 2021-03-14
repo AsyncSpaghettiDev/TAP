bc0fbbac0fb6f4f43a93adf16cf91c2d87392bf4
JonathanMov <jafetmv21@gmail.com>
2021-03-13 23:34:12 -0800

    Documentacion

--- BotonEventos/Program.cs
+++ BotonEventos/Program.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 namespace BotonEventos {
     class Program {
         static void Main() {
+            // Se ejecuta la form, ajustando la posicion inicial al centro de la pantalla
             Application.Run(new Lienzo() {
                 Size = new Size(500, 500),
                 BackColor = Color.Gray,
@@ -14,9 +15,11 @@ namespace BotonEventos {
     }
     class Lienzo : Form {
         readonly BotonMov btn;
+        // Se crea un boton con movimientos en la forma
         public Lienzo() {
             this.btn = new BotonMov(this) {};
             this.Controls.Add(this.btn);
+            // Se da la orden de moverse al boton.
             this.btn.mover();
         }
     }
